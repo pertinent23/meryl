6a5c768f32175447069f928efd441d899dc6a976
Franck Duval <10franckpertinent@gmail.com>
2021-07-07 14:26:41 +0100

    end

--- public/js/style.js
+++ public/js/style.js
@@ -1,7 +1,4 @@
 Digital( function () {
     $( '#menu' ).remove();
-    $( '#navbar' ).css( {
-        height: ( $( '#navbar' ).offsetHeight() + 100 ) + 'px'
-    } )
     $( '.main-container' ).css( 'border-top', 'none' );
 } );
